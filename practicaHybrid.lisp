0977e47697115088d46a0a5172649de44de0b2cf
oscarvx00 <oscarvicente@usal.es>
2021-10-02 15:26:20 +0200

    Con la funciones literalmente copiadas funciona con nuestras variables, el error estaria en la funcion top o inificate init o unificate

--- practicaHybrid.lisp
+++ practicaHybrid.lisp
@@ -15,14 +15,16 @@
 ;;; required by the strict definition of unification.
 
 ;;; UNIFY is the top-level function.
-(defun unificate-init (e1 e2)
-
-    (let ((u nil))
-        (if (eql (first e1) (first e2))
-            (unificate e1 e2 u)
-        )
-    )
-)
+(defun unify (literal1 literal2)
+  "Returns a most-general unifier for LITERAL1 and
+   LITERAL2 if one exists.  Otherwise returns the
+   symbol NOT-UNIFIABLE."
+  (let ((u nil))   ; unifier is initially null.
+    ; make sure predicate symbols match:
+    (if (equalp (first literal1) (first literal2))
+        (catch 'unify
+          (unify1 (rest literal1) (rest literal2) u) )
+       'not-unifiable) ) )
 
 (defun miembro (e1 e2)
     (unless (atom e2)
@@ -30,27 +32,33 @@
     )
 )
 
-(defun unificate (e1 e2 u)
+(defun unify1 (termlist1 termlist2 u)
+  "Tries to unify the terms in TERMLIST1 with those in
+   TERMLIST2 and thereby extend the list U of substitutions.
+   If any failure occurs, throws NOT-UNIFIABLE."
+  (cond
+    ; If equal, no substitution necessary:
+    ((equalp termlist1 termlist2) u)
+    ; Check for list length mismatch (a syntax error):
+    ((or (null termlist1) (null termlist2))
+     (throw 'unify 'not-unifiable) )
+    ; If TERMLIST1 is a variable, try to add a substitution:
+    ((variablep termlist1) (add-pair termlist2 termlist1 u))
+    ; Handle the case when TERMLIST2 is a variable similarly:
+    ((variablep termlist2) (add-pair termlist1 termlist2 u))
+    ; Now, if either expression is atomic, it is a
+    ; constant and there's no match since they're not equal:
+    ((or (atomp termlist1) (atomp termlist2))
+     (throw 'unify 'not-unifiable) )
+    ; The expressions must be non-atomic; do recursively.
+    ; Apply current substitutions before unifying the first of each.
+    (t (setf u (unify1 (do-subst (first termlist1) u)
+                       (do-subst (first termlist2) u) 
+                       u) )
+       ; Now unify the rest of each.
+       (unify1 (rest termlist1) (rest termlist2) u) )
+       ) )
 
-    (cond
-        ((atomp e1)
-            (top e1 e2 u)
-        )
-        ((atomp e2)
-            (top e2 e1 u)
-        )
-        (T
-            (setf u (unificate (do-subst (first e1) u) (do-subst (first e2) u) u))
-            (if (equalp u 'error) 
-                'error
-            )
-            (unificate (rest e1) (rest e2) u)
-            (if (equalp u 'error) 
-                'error
-            )
-        )
-    )
-)
 
 (defun atomp (s)
 
@@ -146,14 +154,11 @@
 (defparameter e1 '(p (f a (? x)) (? z)))
 (defparameter e2 '(p (? y) ((? w) d)))
 
-(defparameter e1No '(p (f a x) z))
-(defparameter e2No '(p y (w d)))
-
 ;;; Here's a function for demonstrating UNIFY.
 (defun show-unification (lit1 lit2)
   "Prints out both inputs and output from UNIFY."
   (format t "~%Result of UNIFY on ~s and ~s is ~s."
-          lit1 lit2 (unificate-init lit1 lit2) ) )
+          lit1 lit2 (unify lit1 lit2) ) )
 
 (defun test ()
   "Calls UNIFY with sample arguments."
@@ -161,7 +166,7 @@
   (show-unification *literal3* *literal4*)
   (show-unification *literal5* *literal6*)
   (show-unification *literal7* *literal8*)
-  (let ((u (unificate-init *literal7* *literal8*)))
+  (let ((u (unify *literal7* *literal8*)))
     (format t "~%Result of applying U to ~s is ~s."
             *literal7* (do-subst *literal7* u) )
     (format t "~%Result of applying U to ~s is ~s."
