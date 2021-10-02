fa876ebc296955082976c75500430969567a24c0
oscarvx00 <oscarvicente@usal.es>
2021-10-03 00:30:32 +0200

    Operacion turnitin completada

--- practicaHybrid.lisp
+++ practicaHybrid.lisp
@@ -1,152 +1,123 @@
-
-;;; UNIFY.CL
-;;; A Unification algorithm for literals in the Predicate Calculus,
-;;; implemented in Common Lisp.
-
-;;; (C) Copyright 1995 by Steven L. Tanimoto.
-;;; This program is described in Chapter 6 ("Logical Reasoning") of
-;;; "The Elements of Artificial Intelligence Using Common Lisp," 2nd ed.,
-;;; published by W. H. Freeman, 41 Madison Ave., New York, NY 10010.
-;;; Permission is granted for noncommercial use and modification of
-;;; this program, provided that this copyright notice is retained
-;;; and followed by a notice of any modifications made to the program.
-
-;;; Note: This function UNIFY does indeed perform the "occurs check"
-;;; required by the strict definition of unification.
-
-;;; UNIFY is the top-level function.
-(defun unify (literal1 literal2)
-  "Returns a most-general unifier for LITERAL1 and
-   LITERAL2 if one exists.  Otherwise returns the
-   symbol NOT-UNIFIABLE."
-  (let ((u nil))   ; unifier is initially null.
-    ; make sure predicate symbols match:
-    (if (equalp (first literal1) (first literal2))
-        (catch 'unify
-          (unify1 (rest literal1) (rest literal2) u) )
-       'not-unifiable) ) )
-
-(defun miembro (e1 e2)
-    (unless (atom e2)
-        (member e1 e2)
-    )
+(defun unificar-init (entrada1 entrada2)
+  (let ((theta nil))  ;Iniciamos el unificador a null
+    (if (equalp (first entrada1) (first entrada2))
+      (unificar (rest entrada1) (rest entrada2) theta)
+      'error 
+    ) 
+  ) 
 )
 
-(defun unify1 (termlist1 termlist2 u)
-  "Tries to unify the terms in TERMLIST1 with those in
-   TERMLIST2 and thereby extend the list U of substitutions.
-   If any failure occurs, throws NOT-UNIFIABLE."
+(defun miembro (entrada lista) ;Devuelve T si entrada se encuentra en la lista
+  (cond 
+    ((equalp entrada lista) t)
+    ((atomp lista) nil)
+    (t (or (miembro entrada (first lista))
+            (miembro entrada (rest lista)) 
+        )
+    ) 
+  ) 
+)
+
+(defun unificar (e1 e2 theta)
   (cond
-    ((or (equalp termlist1 'error) (equalp termlist2 'error))
+    ((or (equalp e1 'error) (equalp e2 'error))
       'error
     )
-    ((atomp termlist1)
-      (top termlist1 termlist2 u)
+    ((atomp e1)
+      (top e1 e2 theta)
     )
-    ((atomp termlist2)
-      (top termlist2 termlist1 u)
+    ((atomp e2)
+      (top e2 e1 theta)
     )
-    (t (setf u (unify1 (do-subst (first termlist1) u)
-                       (do-subst (first termlist2) u) 
-                       u) )
-        (if (equalp u 'error)
+    (t (setf theta (unificar (aplicarsustitucion (first e1) theta)
+                       (aplicarsustitucion (first e2) theta) 
+                       theta) )
+        (if (equalp theta 'error)
           'error
         )
        ; Now unify the rest of each.
-       (unify1 (rest termlist1) (rest termlist2) u) )
-       ) )
+       (unificar (rest e1) (rest e2) theta) 
+    )
+  ) 
+)
 
-
-(defun top (e1 e2 u)
+(defun top (e1 e2 theta)
   (cond
-    ((equalp e1 e2) u)
+    ((equalp e1 e2) theta)
     ((or (null e1) (null e2))
       'error
     )
     ((variablep e1) ;si e1 es variable
       (if (miembro e1 e2)
         'error ; return error si es miembro
-        (add-pair e2 e1 u)
+        (anadir e2 e1 theta)
       )
     )
     ((variablep e2)
-      (add-pair e1 e2 u)
+      (anadir e1 e2 theta)
     )
-    ;((atomp e1))
     (t 'error)   
   )
-);fin defun
+)
 
 (defun atomp (s)
-
     (cond((atom s) T)
     ((eq (first s) '?) T)
     (t NIL)
   )
-
 )
 
+(defun anadir (termino variable theta)
+  (cons (list termino variable) 
+    (sustituirexpresion termino variable theta)   
+  )
+)
 
-
-
-(defun add-pair (term variable u)
-  "Trys to add the pair (TERM VARIABLE) to the list U and
-   return the new list.  Otherwise throws NOT-UNIFIABLE."
-  (cons (list term variable) 
-                 (subst1 term variable u) ))
-
-
-;;; DO-SUBST performs all substitutions in L on EXP in
-;;; reverse order.
-
-
-
-(defparameter *s2* '( (a x) (y z) ((f (h)) k) ))
-(defparameter *p2* '( x g (k) f2 (z)))
-
-(defparameter *s3* '( (a (? x)) ((? y) (? z)) ((f (h)) k) ))
-(defparameter *p3* '( (? x) g (k) f2 ((? z))))
-
-
-
-(defun do-subst (exp l)
-  "Applies the substitutions of L to EXP."
-  (cond ((null l) exp)
-        ((equalp l 'error)
-          'error
-        )
-        (t (subst1 (first (first l)) ; (first l)= ((sustitusor)(sustituyendo))
-                   (second (first l))
-                   (do-subst exp (rest l)) )) ) ) ;recursivamente vuelve para sustituir segun las siguientes reglas noseke
-
-(defun subst1 (a b lst)
-; subst1( (? c) (? b) ((P (? b) (? c)) )
-  "Substitutes A for each occurrence of B in LST."
-    (cond
-       ((null lst) nil)
-       ((equalp lst b) a)
-       ((atomp lst) lst)
-       ((equalp b (first lst))
-        (cons a (subst1 a b (rest lst))) )
-       ((atomp (first lst))
-        (cons (first lst)(subst1 a b (rest lst))) )
-       (t (cons (subst1 a b (first lst))
-                (subst1 a b (rest lst)) )) ) )
-
-
-(defun variablep (s)
-  "Returns T if S is a known variable. Es el equivalente al antiguo is_var"
-  (if (atom s) nil ;si es atomo devolver nil
-      (if ;si no lo es,
-        (and
-          (eq (first s) '?)
-          (eq (length s) 2)
-        ) t  ;y es una lista de len 2 con ? de primer argumento, ES UNA VARIABLE
-        nil ;en cualquier otro caso, NO ES VARIABLE
-      )
+(defun aplicarsustitucion (expresion lista)  
+  (cond 
+    ((null lista) expresion)
+    ((equalp lista 'error)
+      'error
     )
-  ;(member s '(x y z x1 x2 y1 y2 z1 z2 u v w)) )
+    (t 
+      (sustituirexpresion (first (first lista)) 
+        (second (first lista))
+        (aplicarsustitucion expresion (rest lista)) 
+      )
+    ) 
+  ) 
 )
+
+(defun sustituirexpresion (a b lista)
+  (cond
+    ((null lista) nil)
+    ((equalp lista b) a)
+    ((atomp lista) lista)
+    ((equalp b (first lista))
+    (cons a (sustituirexpresion a b (rest lista))) )
+    ((atomp (first lista))
+    (cons (first lista)(sustituirexpresion a b (rest lista))) )
+    (t (cons (sustituirexpresion a b (first lista))
+      (sustituirexpresion a b (rest lista)) 
+      )
+    ) 
+  ) 
+)
+
+
+(defun variablep (x)
+  (if (atom x) nil ;si es atomo devolver nil
+    (if ;si no lo es,
+      (and
+        (eq (first x) '?)
+        (eq (length x) 2)
+      ) t  ;y es una lista de len 2 con ? de primer argumento, ES UNA VARIABLE
+      nil ;en cualquier otro caso, NO ES VARIABLE
+    )
+  )
+)
+
+
 ;;; Here is some test data:
 (defparameter *literal1* '(p (? x) (f a)))
 (defparameter *literal2* '(p b (? y)))
@@ -164,7 +135,9 @@
 (defun show-unification (lit1 lit2)
   "Prints out both inputs and output from UNIFY."
   (format t "~%Result of UNIFY on ~s and ~s is ~s."
-          lit1 lit2 (unify lit1 lit2) ) )
+    lit1 lit2 (unificar-init lit1 lit2)
+  )
+)
 
 (defun test ()
   "Calls UNIFY with sample arguments."
@@ -172,11 +145,14 @@
   (show-unification *literal3* *literal4*)
   (show-unification *literal5* *literal6*)
   (show-unification *literal7* *literal8*)
-  (let ((u (unify *literal7* *literal8*)))
+  (show-unification e1 e2)
+
+  (let ((theta (unificar-init *literal7* *literal8*)))
     (format t "~%Result of applying U to ~s is ~s."
-            *literal7* (do-subst *literal7* u) )
+            *literal7* (aplicarsustitucion *literal7* theta) )
     (format t "~%Result of applying U to ~s is ~s."
-            *literal8* (do-subst *literal8* u) )
+            *literal8* (aplicarsustitucion *literal8* theta) )
   ) )
+  
 
 (test)
